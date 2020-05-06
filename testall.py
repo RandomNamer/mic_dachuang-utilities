e1c78eb08024d30f6b722847798a818991cf271e
ZZY <zzy@Matebook-X-Pro.local>
2020-05-06 13:49:34 +0800

    Changes

--- testall.py
+++ testall.py
@@ -1,16 +1,17 @@
 import os
 from glob import glob
 
-work_dir='/home/mic_dachuang/B/mmdetection/work_dir_new_4_14/ssd512_default'
-config='/home/mic_dachuang/B/mmdetection/modified_configs/ssd512_coco.py'
+work_dir='/home/mic_dachuang/mmdetection/B/work_dir/faster_rcnn_48epoch'
+config='/home/mic_dachuang/mmdetection/B/faster_rcnn_r50_fpn_1x.py'
 
-f=open(work_dir+'testall.txt','w+')
+f=open(work_dir+'/testall.txt','w+')
 content=''
 for epoch in glob(work_dir+'/*.pth'):
-    print("Testing "+epoch+'...')
-    raw=os.popen('pyhton tools/test.py '+config+' '+epoch+' --eval bbox').readlines()
-    result=raw[-12:0]
+    print('Testing ',epoch,'...')
+    raw=os.popen('python tools/test.py '+config+' '+epoch+' --eval bbox').readlines()
+    result=raw[-12:]
     print(result)
-    content=content+result+'\n'
+    content=content+epoch+':\n'+str(result)+'\n'
 f.write(content)
 f.flush()
+f.close()
